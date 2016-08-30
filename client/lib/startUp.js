507ff7455860fa9de76efea40f7b8e1afa7923c7
Connor Hallett <connor.hallett@gmail.com>
2016-08-30 16:00:18 -0400

    Update startUp.js

--- client/lib/startUp.js
+++ client/lib/startUp.js
@@ -6,40 +6,4 @@ Meteor.startup(function () {
     Questions = new Ground.Collection('questions', { connection: null });
     Answers = new Ground.Collection('answers', { connection: null });
     Categories = new Ground.Collection('categories', { connection: null });
-
-    Session.set('data', [
-        {
-            "id": "FIS",
-            "order": "1.1",
-            "score": "7",
-            "weight": "1",
-            "color": "#4D9DB4",
-            "label": "Fisheries",
-            extrafield: "test"
-        },
-        {
-            "id": "MAR",
-            "order": "1.3",
-            "score": "24",
-            "weight": "1",
-            "color": "#C32F4B",
-            "label": "Mariculture"
-        },
-        {
-            "id": "AO",
-            "order": "1.3",
-            "score": "54",
-            "weight": "1",
-            "color": "#E1514B",
-            "label": "Artisanal Fishing Opportunities"
-        },
-        {
-            "id": "YE",
-            "order": "1.3",
-            "score": "61",
-            "weight": "1",
-            "color": "#FB9F59",
-            "label": "fourth One"
-        }
-    ])
 });
